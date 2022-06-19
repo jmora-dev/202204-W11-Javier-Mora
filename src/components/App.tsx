3c65407f86b3235f0b00847cc7544aa263995e81
jmora-dev <dam2mora@gmail.com>
2022-06-19 13:12:16 +0200

    Refactor gentleman list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,53 +1,15 @@
-import { useState } from "react";
 import GentlemanList from "./GentlemanList";
-import { gentlemanDataList } from "../data/gentlemanDataList";
-import SelectAllButton from "./SelectAllButton";
 
 import "../css/styles.css";
 
 export default function App() {
-  const [dataList, setDataList] = useState(gentlemanDataList);
-
-  const selectAll = (newSelectedValue: boolean): void => {
-    setDataList(
-      dataList.map((data) => ({ ...data, selected: newSelectedValue }))
-    );
-  };
-
-  const deleteById = (id: number): void => {
-    setDataList(dataList.filter((data) => data.id !== id));
-  };
-
-  const selectById = (id: number): void => {
-    setDataList(
-      dataList.map((data) => ({
-        ...data,
-        selected: data.id === id ? !data.selected : data.selected,
-      }))
-    );
-  };
-
   return (
     <div className="container">
       <header className="main-header">
         <h1 className="main-title">The pointing gentlemen</h1>
       </header>
-      <section className="controls">
-        <p className="info">
-          {dataList.filter((data) => data.selected).length} gentlemen pointing
-          at you
-        </p>
-        <SelectAllButton
-          isAllSelected={dataList.every((data) => data.selected)}
-          selectAll={selectAll}
-        />
-      </section>
       <main className="main">
-        <GentlemanList
-          dataList={dataList}
-          selectById={selectById}
-          deleteById={deleteById}
-        />
+        <GentlemanList />
       </main>
     </div>
   );
---
+++ src/services/gentlemanJsonServer.ts
@@ -0,0 +1,22 @@
+import { iGentleman } from "../interfaces/iGentleman";
+
+const url = "http://localhost:3356/gentleman";
+
+export const getAllGentleman = (): Promise<Array<iGentleman>> => {
+  return fetch(url).then((res) => res.json());
+};
+
+export const deleteGentleman = (id: number): Promise<iGentleman> => {
+  return fetch(url + id, { method: "DELETE" }).then((res) => res.json());
+};
+
+export const updateGentleman = (
+  id: number,
+  update: Partial<iGentleman>
+): Promise<iGentleman> => {
+  return fetch(url + `/${id}`, {
+    method: "PATCH",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(update),
+  }).then((res) => res.json());
+};
